8720254e736f707b0450905053ec7ee5c5c715a6
morimorig3 <mori.nto@outlook.jp>
2022-01-15 13:48:42 +0900

    レイアウトを作成

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,13 +1,23 @@
 import { FunctionComponent, ComponentChildren } from "preact";
-import { Container, Text } from "@chakra-ui/react";
+import { Container, HStack, VStack, Box, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 type Props = {
   children: ComponentChildren;
 };
 
 export const Layout: FunctionComponent<Props> = ({ children }) => (
-  <Container>
-    <Text>テキスト</Text>
-    <Text>あああああ</Text>
+  <Container maxW="container.lg">
+    <VStack minHeight="100vh" px={5}>
+      <Box w="full" flex={1} py={10}>
+        {children}
+      </Box>
+      <HStack>
+        <Link href="https://www.morimorig3.com/">morimorig3.com</Link>
+        <Link href="https://github.com/morimorig3/weather-app" isExternal>
+          Github <ExternalLinkIcon mx="2px" />
+        </Link>
+      </HStack>
+    </VStack>
   </Container>
 );
